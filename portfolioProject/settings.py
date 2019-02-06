a3ff886e4029a919d7006367c06f2ec29f2135e5
Rahul Jain <rahuldkjain@gmail.com>
2019-02-06 18:55:16 +0530

    PostgreSQL database added & connected

--- portfolioProject/settings.py
+++ portfolioProject/settings.py
@@ -76,8 +76,20 @@ WSGI_APPLICATION = 'portfolioProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # for sqlite database
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        #for postgresql
+        # 'NAME': <db_name>
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Rahul@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+
     }
 }
 
