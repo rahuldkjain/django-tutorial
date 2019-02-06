7aa0c26a5595d184d800ce96a9f14116e6b17a4f
Rahul Jain <rahuldkjain@gmail.com>
2019-02-06 19:44:44 +0530

    homepage settings up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.DateField()
+    body = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='images/')
